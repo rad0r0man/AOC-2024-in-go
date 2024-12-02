7afed66c187b57809c8f1acb19e004fe9fd44396
Rad0Roman <radomir@injurymap.com>
2024-12-02 11:20:04 +0100

    Solve day-1, preliminary solution

---
+++ day-1/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	filePath := "puzzle-input.txt"
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	var leftSideList []int
+	var rightSideList []int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Fields(line)
+
+		if len(parts) == 2 {
+			intFirstNumber, err := strconv.Atoi(parts[0])
+			if err != nil {
+				fmt.Println("Error converting number:", err)
+				return
+			}
+			intSecondNumber, err := strconv.Atoi(parts[1])
+			if err != nil {
+				fmt.Println("Error converting number:", err)
+				return
+			}
+
+			leftSideList = append(leftSideList, intFirstNumber)
+			rightSideList = append(rightSideList, intSecondNumber)
+
+			sort.Ints(leftSideList)
+			sort.Ints(rightSideList)
+
+		}
+	}
+	fmt.Print("Left side: ", leftSideList)
+	fmt.Print("Right side: ", rightSideList)
+
+	var distance int
+	for i, v := range leftSideList {
+		fmt.Println("index: ", i, "value left: ", v, "value right: ", rightSideList[i])
+		if v == rightSideList[i] {
+			continue
+		}
+		if v > rightSideList[i] {
+			distance += v - rightSideList[i]
+			continue
+		}
+		if v < rightSideList[i] {
+			distance += rightSideList[i] - v
+			continue
+		}
+	}
+	fmt.Println("final distance: ", distance)
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rad0r/AOC-2024-in-go
+
+go 1.23.2
